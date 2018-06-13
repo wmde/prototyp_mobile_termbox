842f43d114c1276b916b98a5d461be494e706089
addshore <addshorewiki@gmail.com>
2018-06-13 08:40:40 +0100

    eslint: ShowMoreLanguagesMenuBarLanguagesFilter
    Fixes most issues except for the one no line number is
    reported for...

--- src/components/termbox/ShowMore/ShowMoreLanguagesMenuBarLanguagesFilter.vue
+++ src/components/termbox/ShowMore/ShowMoreLanguagesMenuBarLanguagesFilter.vue
@@ -7,7 +7,7 @@ export default {
 		menuSwitch: Object
 	},
 	data: function () {
-		return { reset: true, originLanguages: [], lastPosition: 0, include:'', isDiabled: false };
+		return { reset: true, originLanguages: [], lastPosition: 0, include: '', isDiabled: false };
 	},
 	mounted: function () {
 		this.$data.reset = true;
@@ -42,43 +42,35 @@ export default {
 			return this.pushVisibleLanguages( 'otherLanguages' );
 		},
 		getTopLanguages() {
-			return this.$props.languagesSettings.get( 'otherLanguages' )
+			return this.$props.languagesSettings.get( 'otherLanguages' );
 		},
 		getPossibleLanguages() {
 			return this.pushVisibleLanguages( 'possibleLanguages' );
 		},
 		getLanguageNames: function () {
 			return this.$props.languagesSettings.get( 'languageNames' );
-		},
+		}
 	},
 	methods: {
 		shouldBeDisabled: function ( Language ) {
-			return this.$props.languagesSettings.get( 'currentLanguage' ) === Language
+			return this.$props.languagesSettings.get( 'currentLanguage' ) === Language;
 		},
-		pushVisibleLanguages(Key)
-		{
-			var Index
-			const Output = []
-			if( 0 === this.$data.include.length )
-			{
+		pushVisibleLanguages( Key ) {
+			let Index;
+			const Output = [];
+			if ( 0 === this.$data.include.length ) {
 				return this.$props.languagesSettings.get( Key );
 			}
-			else
-			{
-				for( Index in this.$props.languagesSettings.get( Key ) )
-				{
-					if( this.$props.languagesSettings.get( Key )[Index] in this.$props.languagesSettings.get( 'languageNames' )
-					&&
-						true === this.$props.languagesSettings.get( 'languageNames' )[
-							this.$props.languagesSettings.get( Key )[Index]
-							].startsWith( this.$data.include )
-					)
-					{
-						Output.push( this.$props.languagesSettings.get( Key )[Index] )
+			else {
+				for ( Index in this.$props.languagesSettings.get( Key ) ) {
+					if ( this.$props.languagesSettings.get( Key )[ Index ] in this.$props.languagesSettings.get( 'languageNames' ) &&
+						true === this.$props.languagesSettings.get( 'languageNames' )[ this.$props.languagesSettings.get( Key )[ Index ] ].startsWith( this.$data.include )
+					) {
+						Output.push( this.$props.languagesSettings.get( Key )[ Index ] );
 					}
 				}
 
-				return Output
+				return Output;
 			}
 		},
 		isInActiveLanguage: function ( Language ) {
@@ -94,7 +86,7 @@ export default {
 		},
 		selectLanguage: function ( Language ) {
 			this.$props.languagesSettings.get( 'otherLanguages' ).push( Language );
-			this.$data.isDiabled = false
+			this.$data.isDiabled = false;
 			this.$forceUpdate();
 		},
 		unSelectLanguage: function ( Language ) {
@@ -103,15 +95,12 @@ export default {
 					this.$props.languagesSettings.get( 'otherLanguages' ).indexOf( Language ),
 					1
 				);
-				if( 1 === this.$props.languagesSettings.get( 'otherLanguages' ).length )
-				{
-					this.$data.isDiabled = true
+				if ( 1 === this.$props.languagesSettings.get( 'otherLanguages' ).length ) {
+					this.$data.isDiabled = true;
 				}
 				this.$forceUpdate();
-			}
-			else
-			{
-				this.$data.isDiabled = true
+			} else {
+				this.$data.isDiabled = true;
 				this.$forceUpdate();
 			}
 		}
@@ -120,219 +109,198 @@ export default {
 </script>
 
 <template>
-    <div id="showMoreLanguagesLanguagesFilterBox">
-        <div id="showMoreLanguagesActiveLanguages">
-            <span v-bind:key="language" v-for="language in getTopLanguages">{{getLanguageNames[language]}}</span>
-        </div>
-        <div class="showMoreLanguagesLanguagesFilter">
-            <button @click="close()"
-                    class="showMoreLanguagesMenuLanguageFilterSave"
-            >Remember selected languages</button>
-            <div @click="activateTypeFilter()"
-                 class="showMoreLanguagesMenuTypeFilterActivator">
-                <button><img src="../../../assets/Bars.png" /></button>
-            </div>
-        </div>
-        <form id="showMoreLanguagesLanguagesFilterMenu">
-            <div id="showMoreLanguagesSearchBar">
-                <div>
-                    <input type="text" v-model="include" placeholder="Find language"/>
-                </div>
-                <button><img src="../../../assets/Lupe.png" /></button>
-            </div>
-            <div id="showMoreLanguagesLanguagesSelection">
-                <div v-bind:key="language"
-					 v-for="language in getLanguages"
-					 class="showMoreLanguagesLanguagesActiveLanguage">
+	<div id="showMoreLanguagesLanguagesFilterBox">
+		<div id="showMoreLanguagesActiveLanguages">
+			<span v-bind:key="language" v-for="language in getTopLanguages">{{getLanguageNames[language]}}</span>
+		</div>
+		<div class="showMoreLanguagesLanguagesFilter">
+			<button @click="close()"
+					class="showMoreLanguagesMenuLanguageFilterSave"
+			>Remember selected languages
+			</button>
+			<div @click="activateTypeFilter()"
+				class="showMoreLanguagesMenuTypeFilterActivator">
+				<button><img src="../../../assets/Bars.png"/></button>
+			</div>
+		</div>
+		<form id="showMoreLanguagesLanguagesFilterMenu">
+			<div id="showMoreLanguagesSearchBar">
+				<div>
+					<input type="text" v-model="include" placeholder="Find language"/>
+				</div>
+				<button><img src="../../../assets/Lupe.png"/></button>
+			</div>
+			<div id="showMoreLanguagesLanguagesSelection">
+				<div v-bind:key="language"
+					v-for="language in getLanguages"
+					class="showMoreLanguagesLanguagesActiveLanguage">
 					<div v-if="shouldBeDisabled(language)">
-                    	<input id="lastStanding" disabled checked type="checkbox"/>
-                    	<label>{{getLanguageNames[getLanguages[0]]}}</label>
+						<input id="lastStanding" disabled checked type="checkbox"/>
+						<label>{{getLanguageNames[getLanguages[0]]}}</label>
 					</div>
 					<div v-else
-						 @click="unSelectLanguage(language)">
-                    	<input checked type="checkbox"/>
-                    	<label>{{getLanguageNames[language]}}</label>
+						@click="unSelectLanguage(language)">
+						<input checked type="checkbox"/>
+						<label>{{getLanguageNames[language]}}</label>
 					</div>
-                </div>
-                <div class="showMoreLanguagesLanguagesInActiveLanguage"
-                     v-if="isInActiveLanguage(language)"
-                     v-bind:key="language"
-                     v-for="language in getPossibleLanguages"
-                     @click="selectLanguage(language)" >
+				</div>
+				<div class="showMoreLanguagesLanguagesInActiveLanguage"
+					v-if="isInActiveLanguage(language)"
+					v-bind:key="language"
+					v-for="language in getPossibleLanguages"
+					@click="selectLanguage(language)">
 					<div>
-                    	<input type="checkbox"/>
-                    	<label>{{getLanguageNames[language]}}</label>
+						<input type="checkbox"/>
+						<label>{{getLanguageNames[language]}}</label>
 					</div>
-                </div>
-            </div>
-        </form>
-    </div>
+				</div>
+			</div>
+		</form>
+	</div>
 </template>
 
 <style scoped>
-#showMoreLanguagesLanguagesFilterBox
-{
-    position: absolute;
-    top:0;
-    overflow-x: hidden;
-    width:100%;
+#showMoreLanguagesLanguagesFilterBox {
+	position: absolute;
+	top: 0;
+	overflow-x: hidden;
+	width: 100%;
 	height: 100%;
-	background-color:#eaecf0;
+	background-color: #eaecf0;
 }
 
-.showMoreLanguagesLanguagesFilter
-{
-    height: 50px;
-    background-color: #3366cc;
-    border-color: #f5f5f5;
-    border-bottom-width: 2px;
-    border-bottom-color: #a2a9b1;
-    border-bottom-style: solid;
+.showMoreLanguagesLanguagesFilter {
+	height: 50px;
+	background-color: #3366cc;
+	border-color: #f5f5f5;
+	border-bottom-width: 2px;
+	border-bottom-color: #a2a9b1;
+	border-bottom-style: solid;
 }
 
-.showMoreLanguagesMenuLanguageFilterSave
-{
-    background-color: #3366cc;
-    color: #ffffff;
-    display: inline;
-    border-style: none;
-    padding-top: 3px;
-    padding-left: 15px;
-    font-size:1em;
-    width: 100%;
-    height: 50px;
+.showMoreLanguagesMenuLanguageFilterSave {
+	background-color: #3366cc;
+	color: #ffffff;
+	display: inline;
+	border-style: none;
+	padding-top: 3px;
+	padding-left: 15px;
+	font-size: 1em;
+	width: 100%;
+	height: 50px;
 }
 
-.showMoreLanguagesLanguagesFilter > div
-{
-    background-color: #c8ccd1;
-    border-left-width: 0px;
+.showMoreLanguagesLanguagesFilter > div {
+	background-color: #c8ccd1;
+	border-left-width: 0px;
 }
 
-.showMoreLanguagesLanguagesFilter > div > img
-{
-    padding-left: 8.5px;
-    padding-right: 8.5px;
+.showMoreLanguagesLanguagesFilter > div > img {
+	padding-left: 8.5px;
+	padding-right: 8.5px;
 }
 
-#showMoreLanguagesActiveLanguages
-{
-    text-align: left;
-    background-color: #f8f9fa;
-    color: #696d71;
-    padding-top: 12.5px;
-    padding-bottom: 12.5px;
-    width: 100%;
-    margin-left: 5%;
-    padding-right: 5%;
-    line-height: 1.5;
+#showMoreLanguagesActiveLanguages {
+	text-align: left;
+	background-color: #f8f9fa;
+	color: #696d71;
+	padding-top: 12.5px;
+	padding-bottom: 12.5px;
+	width: 100%;
+	margin-left: 5%;
+	padding-right: 5%;
+	line-height: 1.5;
 }
 
-#showMoreLanguagesActiveLanguages > span
-{
-    display: inline-block;
-    margin-left: 30px;
+#showMoreLanguagesActiveLanguages > span {
+	display: inline-block;
+	margin-left: 30px;
 }
 
-#showMoreLanguagesActiveLanguages
-{
-    display: inline-block;
-    margin-left: 0px!important;
+#showMoreLanguagesActiveLanguages {
+	display: inline-block;
+	margin-left: 0px !important;
 }
 
-#showMoreLanguagesLanguagesFilterMenu
-{
-    width: 100%;
-    color: #7a7e84;
-    padding-top: 25px;
+#showMoreLanguagesLanguagesFilterMenu {
+	width: 100%;
+	color: #7a7e84;
+	padding-top: 25px;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div
-{
-    padding: 25px 0px 0px 25px;
-    margin-bottom: 0px;
+#showMoreLanguagesLanguagesSelection > div > div {
+	padding: 25px 0px 0px 25px;
+	margin-bottom: 0px;
 }
 
-#showMoreLanguagesLanguagesSelection > div:first-child
-{
+#showMoreLanguagesLanguagesSelection > div:first-child {
 	padding-top: 15px;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div:last-child
-{
-    padding-bottom: 30px;
+#showMoreLanguagesLanguagesSelection > div > div:last-child {
+	padding-bottom: 30px;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div > label
-{
-    display: inline-block;
-    color: #72777d;
-    font-size: 1.3em;
-    padding-right:90px;
+#showMoreLanguagesLanguagesSelection > div > div > label {
+	display: inline-block;
+	color: #72777d;
+	font-size: 1.3em;
+	padding-right: 90px;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div > input[checked]+label
-{
-    color:#000000 !important;
+#showMoreLanguagesLanguagesSelection > div > div > input[checked] + label {
+	color: #000000 !important;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div > input[disabled]+label
-{
-	color:#3366cc !important;
+#showMoreLanguagesLanguagesSelection > div > div > input[disabled] + label {
+	color: #3366cc !important;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div > input
-{
-    display: inline-block;
-    float: right;
-    right: 35px;
-    position: absolute;
-	width:20px;
-	height:20px;
+#showMoreLanguagesLanguagesSelection > div > div > input {
+	display: inline-block;
+	float: right;
+	right: 35px;
+	position: absolute;
+	width: 20px;
+	height: 20px;
 }
 
-#showMoreLanguagesLanguagesSelection > div > div > input[checked]
-{
-    background:#3366cc;
+#showMoreLanguagesLanguagesSelection > div > div > input[checked] {
+	background: #3366cc;
 }
 
-#showMoreLanguagesSearchBar
-{
-    margin: auto;
-    width: 80%;
-    background-color: #ffffff;
-    height:30px;
-    border: 1px solid #a2a9b1;
-    border-radius: 5px;
-    padding: 5px 5px 5px 5px;
+#showMoreLanguagesSearchBar {
+	margin: auto;
+	width: 80%;
+	background-color: #ffffff;
+	height: 30px;
+	border: 1px solid #a2a9b1;
+	border-radius: 5px;
+	padding: 5px 5px 5px 5px;
 }
 
-#showMoreLanguagesSearchBar > div
-{
-    width: 100%;
+#showMoreLanguagesSearchBar > div {
+	width: 100%;
 }
 
-#showMoreLanguagesSearchBar > div > input
-{
-    width: 80%;
-    height: 27px;
-    border-width: 0px!important;
-    padding-left: 2px ;
-	font-size:1.3em;
+#showMoreLanguagesSearchBar > div > input {
+	width: 80%;
+	height: 27px;
+	border-width: 0px !important;
+	padding-left: 2px;
+	font-size: 1.3em;
 }
 
-#showMoreLanguagesSearchBar > button
-{
-    float: right;
-    border-width: 0px!important;
-    background:transparent;
-    top:-30px;
-    margin: auto;
-    position: relative;
+#showMoreLanguagesSearchBar > button {
+	float: right;
+	border-width: 0px !important;
+	background: transparent;
+	top: -30px;
+	margin: auto;
+	position: relative;
 }
 
-#showMoreLanguagesSearchBar > button > img
-{
-    height: 30px;
+#showMoreLanguagesSearchBar > button > img {
+	height: 30px;
 }
 </style>
