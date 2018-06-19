69afcf817c544758288fc946b86c97dcc653ac91
Jonas Kress <jonas.kress@wikimedia.de>
2018-06-19 15:46:16 +0200

    Update ShowMoreLanguagesMenuSwitch.vue

--- src/components/termbox/ShowMore/ShowMoreLanguagesMenuSwitch.vue
+++ src/components/termbox/ShowMore/ShowMoreLanguagesMenuSwitch.vue
@@ -76,7 +76,7 @@ button.showMoreLanguagesMenuLanguageFilterActivator
     color: #425f9a;
     padding-top: 3px;
     padding-left: 30px;
-    font-size:1.1em;
+    font-size:1em;
     width: 100%;
     height: 50px;
 	font-family:'Helvetica Neue','Helvetica','Nimbus Sans L','Arial','Liberation Sans',sans-serif;
